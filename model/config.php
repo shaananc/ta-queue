22d6d26f1e7f1e462888a5e9a386aaa8344afcaf
Shaanan Cohney <shaananc@gmail.com>
2023-01-12 17:55:55 +1100

    add compose file

--- model/config.php
+++ model/config.php
@@ -1,26 +1,25 @@
 <?php
 define("SRV_HOSTNAME", "");
 
-define("LDAP_SERVERS",[]);
+define("LDAP_SERVERS", []);
 define("LDAP_DOMAIN", "");
 define("BIND_USER",   "");
 define("BIND_PASSWD", "");
 define("BASE_OU",     "");
 
-define("SQL_SERVER",  "");
-define("SQL_USER",    "");
-define("SQL_PASSWD",  "");
-define("DATABASE",    "");
+define("SQL_SERVER",  "db");
+define("SQL_USER",    "root");
+define("SQL_PASSWD",  "ghXr74@439-M");
+define("DATABASE",    "taqueue");
 
-define("HELP_EMAIL",  "");
+define("HELP_EMAIL",  "cohneys@unimelb.edu.au");
 
 //Auth must be LDAP or CAS
-define("AUTH", "");
-if(AUTH == "CAS"){
+define("AUTH", "LDAP");
+if (AUTH == "CAS") {
   $phpcas_path = '';
   $cas_host = '';
   $cas_context = '';
   $cas_port = 443;
   $cas_server_ca_cert_path = '';
 }
-?>
